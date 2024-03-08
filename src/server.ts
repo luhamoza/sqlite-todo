189dedf99cd44047442aea8b36a254e9d4294743
luhamoza <luqmanhafizimohdzawawi@gmail.com>
2024-03-09 03:35:32 +0800

    feat: Enable all CORS Requests

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import db from "./config/database.config";
 import todoRouter from "./route";
+import cors from "cors";
 
 db.sync().then(() => {
   console.log("Database is connected");
@@ -9,6 +10,8 @@ db.sync().then(() => {
 const app = express();
 const port = 3000;
 app.use(express.json());
+app.use(cors());
+
 app.use("/api/v1", todoRouter);
 
 app.listen(port, () => {
